cf8fc3ad6d1e67ec5ddc3b98a3514eba37d59ca3
Nicholas <nicholas.griffin@bbc.co.uk>
2024-07-12 22:59:42 +0100

    feat: adding filtering

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,36 @@
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
+import { drizzle } from 'drizzle-orm/d1';
+import { eq, or } from 'drizzle-orm';
+
+import { bronzeDefeatedMps, bronzeElectedMps } from './schema';
+
 export interface Env {
   AUTH_TOKEN: string;
   DB: D1Database;
+}
+
+async function getBronzeMpData(
+  db: DrizzleD1Database,
+  table: typeof bronzeElectedMps | typeof bronzeDefeatedMps,
+  id: string | null,
+  party: string | null,
+  region: string | null,
+  country: string | null,
+  constituency: string | null
+) {
+  return await db
+    .select()
+    .from(table)
+    .where(
+      or(
+        id ? eq(table.id, Number(id)) : undefined,
+        party ? eq(table.party_abbreviation, party) : undefined,
+        region ? eq(table.region_name, region) : undefined,
+        country ? eq(table.country_name, country) : undefined,
+        constituency ? eq(table.constituency_name, constituency) : undefined
+      )
+    )
+    .all();
 }
 
 export default {
@@ -16,11 +46,52 @@ export default {
     }
 
     const { method, url } = request;
-    const { pathname, search, searchParams } = new URL(url);
+    const { pathname, searchParams } = new URL(url);
+
+    const db = drizzle(env.DB);
+
+    const idParam = searchParams.get('id');
+    const partyParam = searchParams.get('party');
+    const regionParam = searchParams.get('region');
+    const countryParam = searchParams.get('country');
+    const constituencyParam = searchParams.get('constituency');
 
     switch (method) {
       case 'GET':
-        return new Response('Hello, world!');
+        switch (pathname) {
+          case '/':
+            return new Response('Hello, world!');
+          case '/bronze-elected-mps':
+            const bronzeElectedMpsResult = await getBronzeMpData(
+              db,
+              bronzeElectedMps,
+              idParam,
+              partyParam,
+              regionParam,
+              countryParam,
+              constituencyParam
+            );
+
+            return new Response(JSON.stringify(bronzeElectedMpsResult), {
+              headers: { 'Content-Type': 'application/json' },
+            });
+          case '/bronze-defeated-mps':
+            const bronzeDefeatedMpsResult = await getBronzeMpData(
+              db,
+              bronzeDefeatedMps,
+              idParam,
+              partyParam,
+              regionParam,
+              countryParam,
+              constituencyParam
+            );
+
+            return new Response(JSON.stringify(bronzeDefeatedMpsResult), {
+              headers: { 'Content-Type': 'application/json' },
+            });
+          default:
+            return new Response('Not Found', { status: 404 });
+        }
       default:
         return new Response('Method not Allowed', { status: 405 });
     }
